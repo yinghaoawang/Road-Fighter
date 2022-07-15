1cb310314a336b8a3406aa7f9a4c9f239b6e86b3
Yinghao Wang <yinghao.a.wang@gmail.com>
2022-07-14 23:53:08 -0700

    sprites and sprite switching added

--- js/player.js
+++ js/player.js
@@ -1,6 +1,6 @@
 class Player extends Sprite {
-    constructor({position={x:50, y:0}, size={x:80, y:150}, speed=8, jumpSpeed=40, velocity={x:0, y:0}}) {
-        super({position, size});
+    constructor({position={x: 0, y: 0}, frameData, targetSize, spriteSize, size={x:80, y:150}, facingRight=true, imageUrl, speed=8, jumpSpeed=40, velocity={x:0, y:0}, sprites}) {
+        super({position, spriteSize, imageUrl, targetSize, frameData, facingRight, sprites});
         this.position = position;
         this.size = size;
         this.speed = speed;
@@ -55,35 +55,47 @@ class Player extends Sprite {
 
         this.position.x += this.velocity.x;
 
-        if (this.size.y + this.position.y + this.velocity.y > canvas.height - 70) {
-            this.position.y = canvas.height - 70 - this.size.y;
+        if (this.size.y / 2 + this.position.y + this.velocity.y > canvas.height - 70) {
+            this.position.y = canvas.height - 70 - this.size.y / 2;
             this.grounded = true;
         } else {
             this.position.y += this.velocity.y;
         }
 
-    }
-
-    getCenter() {
-        return {
-            x: this.position.x + this.size.x / 2,
-            y: this.position.y + this.size.y / 2
+        if (!this.grounded) {
+            if (this.velocity.y > 0) {
+                this.switchSprite('fall');
+            } else {
+                this.switchSprite('jump');
+            }
+        } else {
+            if (this.velocity.x != 0) {
+                this.switchSprite('run');
+            } else {
+                this.switchSprite('idle')
+            }
         }
+
     }
 
     draw() {
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
+        super.draw();
 
         ctx.strokeStyle = 'red';
-        ctx.strokeRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        ctx.strokeRect(this.position.x - this.size.x / 2, this.position.y - this.size.y / 2, this.size.x, this.size.y);
 
         if (this.attack.isAttacking) {
-            ctx.strokeStyle = 'lime';
-            ctx.strokeRect(this.getCenter().x + this.attack.offset.x, this.getCenter().y + this.attack.offset.y,
+            ctx.save();
+            ctx.strokeStyle = 'limegreen';
+            ctx.translate(this.position.x, this.position.y);
+            ctx.scale(this.facingRight ? 1 : -1, 1);
+
+            ctx.strokeRect(this.attack.offset.x, this.attack.offset.y,
                 this.attack.size.x, this.attack.size.y);
-            ctx.fillStyle = 'green';
-            ctx.fillRect(this.getCenter().x + this.attack.offset.x, this.getCenter().y + this.attack.offset.y,
+            ctx.fillStyle = 'lime';
+            ctx.fillRect(this.attack.offset.x, this.attack.offset.y,
                 this.attack.size.x, this.attack.size.y);
+            ctx.restore();
         }
     }
 }
--- js/sprite.js
+++ js/sprite.js
@@ -1,12 +1,71 @@
 class Sprite {
-    constructor({imageUrl='./images/Zebra.png', position, size}) {
+    constructor({imageUrl='./images/Zebra.png', spriteSize, facingRight=true, spriteOffset={x: 0, y: 0}, targetSize=spriteSize, position={x: 0, y: 0}, frameData={maxFrames: 1, frameDuration: 500}, sprites}) {
         this.image = new Image();
         this.image.src = imageUrl;
         this.position = position;
-        this.size = size;
+        if (spriteSize == null) {
+            this.image.onload = () => {
+                this.spriteSize = {
+                    x: this.image.width,
+                    y: this.image.height
+                }
+            };
+        }
+        this.facingRight = facingRight;
+        this.spriteSize = spriteSize;
+        this.spriteOffset = spriteOffset;
+        this.targetSize = targetSize;
+        this.currentFrame = 0;
+        this.frameData = frameData;
+        this.lastFrameDrawn = Date.now();
+        this.sprites = sprites;
+        for (const spriteKey in sprites) {  
+            let sprite = sprites[spriteKey];
+            sprite.image = new Image();
+            sprite.image.src = sprite.imageUrl;
+        }
+    }
+
+    switchSprite(spriteKey) {
+        if (this.sprites == null || this.sprites[spriteKey] == null) {
+            console.error("Sprite does not have key: " + spriteKey);
+            return;
+        }
+
+        if (this.sprites[spriteKey].image == this.image) {
+            return;
+        }
+
+        this.image = this.sprites[spriteKey].image;
+        this.frameData.maxFrames = this.sprites[spriteKey].maxFrames;
+        this.currentFrame = 0;
+    }
+
+    drawSprite({flipped={x: false}}) {
+        ctx.save();
+        ctx.translate(this.position.x + (flipped.x ? this.targetSize.x : 0), this.position.y);
+        ctx.scale(flipped.x ? -1 : 1, 1);
+        ctx.drawImage(this.image,
+            this.currentFrame * this.spriteSize.x, 0, this.spriteSize.x, this.spriteSize.y,
+            (flipped.x ? 1 : -1) * this.targetSize.x / 2, -this.targetSize.y / 2, this.targetSize.x, this.targetSize.y);
+        ctx.restore();
+    }
+
+    animateFrames() {
+        if (Date.now() - this.lastFrameDrawn >= this.frameData.frameDuration) {
+            this.currentFrame++;
+            if (this.currentFrame >= this.frameData.maxFrames - 1) this.currentFrame = 0;
+            this.lastFrameDrawn = Date.now();
+        }
     }
 
     draw() {
-        ctx.drawImage(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
+        if (this.spriteSize == null) return;
+
+        this.animateFrames();
+        this.drawSprite({flipped: {x: !this.facingRight}});
+        
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.position.x - this.targetSize.x / 2, this.position.y - this.targetSize.y / 2, this.targetSize.x, this.targetSize.y);
     }
 }
