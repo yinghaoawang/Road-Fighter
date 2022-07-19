ceaeb473b396c24064a00d9d5bc853b4590afbad
Yinghao Wang <yinghao.a.wang@gmail.com>
2022-07-19 14:21:58 -0700

    wizard animations, sprites, atk hitboxes working

--- js/game.js
+++ js/game.js
@@ -5,7 +5,37 @@ canvas.height = 576;
 
 const gravity = 2.2;
 
-let player = new Player({size: { x: 30, y: 110 },
+let player = new Player({
+    hitbox: {offset: {x: 0, y: -30}, size: { x: 40, y: 100 }},
+    targetSize: {x: 250 * 2, y: 250 * 2},
+    sprites: {
+        idle: {imageUrl: './images/wizard/Idle.png', maxFrames: 8, frameDuration: 100, size: {x: 250, y: 250}},
+        run: {imageUrl: './images/wizard/Run.png', maxFrames: 8, frameDuration: 60, size: {x: 250, y: 250}},
+        fall: {imageUrl: './images/wizard/Fall.png', maxFrames: 2, frameDuration: 100, size: {x: 250, y: 250}},
+        jump: {imageUrl: './images/wizard/Jump.png', maxFrames: 2, frameDuration: 100, size: {x: 250, y: 250}},
+        death: {imageUrl: './images/wizard/Death.png', maxFrames: 7, frameDuration: 100, size: {x: 250, y: 250}},
+        takeHit: {imageUrl: './images/wizard/Take hit.png', maxFrames: 3, frameDuration: 100, size: {x: 250, y: 250}},
+        attack1: {imageUrl: './images/wizard/Attack1.png', maxFrames: 8, frameDuration: 100, size: {x: 250, y: 250}},
+        attack2: {imageUrl: './images/wizard/Attack2.png', maxFrames: 8, frameDuration: 100, size: {x: 250, y: 250}},
+    },
+    attackData: [
+        {
+            hitboxes: [
+                {offset: {x:-80, y: -120}, size: {x: 80, y: 90},},
+                {offset: {x:-80, y: -120}, size: {x: 80, y: 90},},
+                {offset: {x:-80, y: -120}, size: {x: 80, y: 90},},
+                {offset: {x: 40, y: -120}, size: {x: 160, y: 140},},
+                {offset: {x: 40, y: -120}, size: {x: 180, y: 200},},
+                {offset: {x: 40, y: -120}, size: {x: 170, y: 220},},
+                {offset: {x: 40, y: -100}, size: {x: 160, y: 210},},
+                {offset: {x: 0, y: 0}, size: {x: 0, y: 0}},
+            ],
+            spriteName: 'attack2',
+            cooldown: 50
+        }
+    ]
+    /*
+    hitbox: {size: { x: 30, y: 110 }},
     targetSize: {x: 200 * 2, y: 200 * 2},
     sprites: {
         idle: {imageUrl: './images/hero/Idle.png', maxFrames: 4, frameDuration: 100, size: {x: 200, y: 200}},
@@ -28,6 +58,7 @@ let player = new Player({size: { x: 30, y: 110 },
             cooldown: 50
         }
     ]
+    */
 });
 let keysDown = [];
 
